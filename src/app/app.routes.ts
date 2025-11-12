5017e5019c78ef2f95ad51981b5e3cf2b2b6649c
Asuka-Fukuchi <afukuchi24@gmail.com>
2025-11-12 09:46:45 -0800

    251112: add components and modified router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
+import { Home } from './components/home/home';
+import { Project } from './components/project/project';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: Home },
+    { path: "about", component: About },
+    { path: "project", component: Project },
+    { path: "contact", component: Contact }    
+];
